06ef1cba423b41f8125ddd0cdaa4f607cd9fb7d4
Devin Richards <devinmrichards@Devins-MacBook-Pro.local>
2024-05-30 20:49:03 -0700

    create Pantry App with Dart

---
+++ pantry_app/bin/pantry_app.dart
@@ -0,0 +1,5 @@
+import 'package:pantry_app/pantry_app.dart' as pantry_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pantry_app.calculate()}!');
+}
---
+++ pantry_app/test/pantry_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:pantry_app/pantry_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
